b772f46c13a8abc929fcddcb479ad5d4d5d02169
jorisfabi <jorisfabi9@gmail.com>
2025-06-11 19:05:15 +0800

    SearchData.php

--- series crud/searchData.php
+++ series crud/searchData.php
@@ -13,6 +13,30 @@ include 'connect.php';
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
 </head>
 <body>
+    <style>
+        body{
+            background-color:black;
+        }
+       
+        .form-control{
+            background-color:beige;
+             border:2px solid gray;
+            border-radius:7px;
+        }
+        select[name="place"]{
+            border:2px solid gray;
+            border-radius:5px;
+            color:black;
+            background-color:beige;
+            width:120px;
+            height:35px;
+        }
+        .container{
+        border-radius: 25px;
+        background-color: yellow;
+        padding: 50px;
+        }
+    </style>
     <?php
 $data=$_GET['data'];
 $sql="Select * from `seriescrud` where ID=$data";
